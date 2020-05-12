93f40550fbf29c536b3cb902a69af1aa8bdc89bb
ramarimei <56565699+ramarimei@users.noreply.github.com>
2020-05-12 22:19:22 +1200

    test

--- 02-managing-http-requests/02/server.js
+++ 02-managing-http-requests/02/server.js
@@ -1,8 +1,9 @@
 const http = require('http');
+const services = require('../services');
 
 const server = http.createServer();
 server.on('request', (request, response) => {
-  
+    console.log('this is an incoming request');
 });
 
 server.listen(8080);
